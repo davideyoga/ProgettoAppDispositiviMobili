c379fe5610a48dcc6373da3f444a246d1c4c45c2
Unknown <tunyx7@gmail.com>
2018-08-03 18:58:37 +0200

    aaasd

--- uniEventApp/src/pages/login/login.module.ts
+++ uniEventApp/src/pages/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
-import { TranslateModule } from '../../../node_modules/@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
