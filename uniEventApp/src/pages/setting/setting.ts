746a2cc7aea4f6af10eaf0b0a3806e3d298bf3a6
Unknown <tunyx7@gmail.com>
2018-09-11 12:43:44 +0200

    cambio lingua funziona

--- uniEventApp/src/pages/setting/setting.ts
+++ uniEventApp/src/pages/setting/setting.ts
@@ -15,23 +15,30 @@ export class SettingPage {
   utente: User;
   linguaPreferita: string;
   lingue: Array<Lingua>;
+  english:boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserService,
     public linguaService: LinguaService, public translateService: TranslateService) {}
 
   ionViewDidLoad() {
+
     console.log("ionViewDidLoad SettingPage");
     this.lingue = this.linguaService.getLingue();
     this.linguaService.getLinguaAttuale().subscribe((lingua: string) => {
       this.linguaPreferita = lingua;
+      if(lingua=="en") this.english=true;
+      else this.english=false;
     });
     this.userService.getUtente().subscribe((utente: User) => {
       this.utente = utente;
     });
+
   }
 
   cambiaLingua() {
 
+      if (this.english==true) this.linguaPreferita="en";
+      else this.linguaPreferita="it";
       this.translateService.use(this.linguaPreferita);
       this.linguaService.updateLingua(this.linguaPreferita);
 
