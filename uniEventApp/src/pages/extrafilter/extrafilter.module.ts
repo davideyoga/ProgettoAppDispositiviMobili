cd729abcfc1de4511471eda452915ef408e83ac8
Unknown <a.micarelli6@gmail.com>
2018-09-11 22:50:53 +0200

    my events va

--- uniEventApp/src/pages/extrafilter/extrafilter.module.ts
+++ uniEventApp/src/pages/extrafilter/extrafilter.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ExtrafilterPage } from './extrafilter';
-import { TranslateModule } from '@ngx-translate/core';
-
-@NgModule({
-  declarations: [
-    ExtrafilterPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ExtrafilterPage),
-    TranslateModule.forChild()
-  ],
-})
-export class ExtrafilterPageModule {}
+// import { NgModule } from '@angular/core';
+// import { IonicPageModule } from 'ionic-angular';
+// import { ExtrafilterPage } from './extrafilter';
+// import { TranslateModule } from '@ngx-translate/core';
+//
+// @NgModule({
+//   declarations: [
+//     ExtrafilterPage,
+//   ],
+//   imports: [
+//     IonicPageModule.forChild(ExtrafilterPage),
+//     TranslateModule.forChild()
+//   ],
+// })
+// export class ExtrafilterPageModule {}
